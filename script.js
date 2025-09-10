7e5ab2b64de637f50f2313bbc76e33675d67055e
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-09-10 21:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	 const prices = document.querySelectorAll(".price");
+  let total = 0;
+  prices.forEach(price => {
+    total += parseInt(price.textContent);
+  });
+
+  // Create new row
+  const table = document.querySelector("table");
+  const totalRow = document.createElement("tr");
+
+  const totalCell = document.createElement("td");
+  totalCell.setAttribute("colspan", "2");
+  totalCell.textContent = "Total Price: Rs " + total;
+
+  totalRow.appendChild(totalCell);
+  table.appendChild(totalRow);
+
+ 
+  getSumBtn.disabled = true;
 };
+  
 
 getSumBtn.addEventListener("click", getSum);
 
